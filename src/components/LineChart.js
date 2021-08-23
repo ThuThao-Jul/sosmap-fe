31a8373328c48b5a0cde3e1ea3a00c6a1652ccd3
Chuong Nguyen <quyenchuong1998@gmail.com>
2021-08-24 00:05:44 +0700

    display data for line chart

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -4,6 +4,34 @@ import { Line } from "react-chartjs-2";
 import api from "../redux/axios";
 
 const LineChart = () => {
+  const [tickets, setTickets] = useState(0);
+  const [notProcessed, setNotProcessed] = useState({
+    monday: 0,
+    tuesday: 0,
+    wednesday: 0,
+    thursday: 0,
+    friday: 0,
+    saturday: 0,
+    sunday: 0,
+  });
+  const [inProgress, setInProgress] = useState({
+    monday: 0,
+    tuesday: 0,
+    wednesday: 0,
+    thursday: 0,
+    friday: 0,
+    saturday: 0,
+    sunday: 0,
+  });
+  const [done, setDone] = useState({
+    monday: 0,
+    tuesday: 0,
+    wednesday: 0,
+    thursday: 0,
+    friday: 0,
+    saturday: 0,
+    sunday: 0,
+  });
   const data = {
     labels: [
       "Monday",
@@ -16,8 +44,16 @@ const LineChart = () => {
     ],
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
+        label: "Not Processed",
+        data: [
+          notProcessed.monday,
+          notProcessed.tuesday,
+          notProcessed.wednesday,
+          notProcessed.thursday,
+          notProcessed.friday,
+          notProcessed.saturday,
+          notProcessed.sunday,
+        ],
         backgroundColor: [
           "#F7464A",
           `rgba(0, 148, 247, 0.7)`,
@@ -37,8 +73,16 @@ const LineChart = () => {
         borderWidth: 1,
       },
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
+        label: "In Progress",
+        data: [
+          inProgress.monday,
+          inProgress.tuesday,
+          inProgress.wednesday,
+          inProgress.thursday,
+          inProgress.friday,
+          inProgress.saturday,
+          inProgress.sunday,
+        ],
         backgroundColor: [
           "#F7464A",
           `rgba(0, 148, 247, 0.7)`,
@@ -58,8 +102,16 @@ const LineChart = () => {
         borderWidth: 1,
       },
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
+        label: "Done",
+        data: [
+          done.monday,
+          done.tuesday,
+          done.wednesday,
+          done.thursday,
+          done.friday,
+          done.saturday,
+          done.sunday,
+        ],
         backgroundColor: [
           "#F7464A",
           `rgba(0, 148, 247, 0.7)`,
@@ -80,29 +132,143 @@ const LineChart = () => {
       },
     ],
   };
-  const [tickets, setTickets] = useState([]);
-  const [notProcessed, setNotProcessed] = useState([]);
-  const [inProgress, setInProgress] = useState([]);
-  const [done, setDone] = useState([]);
   const getTickets = async () => {
     const res = await api.get("/ticket");
     const tickets = res.data.data;
     setTickets(tickets);
-    const done = tickets.filter((ticket) => ticket.status == "done");
-    setDone(done);
+    const doneDate = {
+      monday: 0,
+      tuesday: 0,
+      wednesday: 0,
+      thursday: 0,
+      friday: 0,
+      saturday: 0,
+      sunday: 0,
+    };
+    const notProcessedDate = {
+      monday: 0,
+      tuesday: 0,
+      wednesday: 0,
+      thursday: 0,
+      friday: 0,
+      saturday: 0,
+      sunday: 0,
+    };
+    const inProgressDate = {
+      monday: 0,
+      tuesday: 0,
+      wednesday: 0,
+      thursday: 0,
+      friday: 0,
+      saturday: 0,
+      sunday: 0,
+    };
+    const done = tickets.filter((ticket) => ticket.status === "done");
+    done.forEach((ticket) => {
+      switch (ticket.date) {
+        case "Monday":
+          doneDate.monday++;
+          break;
+        case "Tuesday":
+          doneDate.tuesday++;
+          break;
+        case "Wednesday":
+          doneDate.wednesday++;
+          break;
+        case "Thursday":
+          doneDate.thursday++;
+          break;
+        case "Friday":
+          doneDate.friday++;
+          break;
+        case "Saturday":
+          doneDate.saturday++;
+          break;
+        case "Sunday":
+          doneDate.sunday++;
+          break;
+        default:
+          break;
+      }
+    });
+    const inProgress = tickets.filter(
+      (ticket) => ticket.status === "in progress"
+    );
+    inProgress.forEach((ticket) => {
+      switch (ticket.date) {
+        case "Monday":
+          inProgressDate.monday++;
+          break;
+        case "Tuesday":
+          inProgressDate.tuesday++;
+          break;
+        case "Wednesday":
+          inProgressDate.wednesday++;
+          break;
+        case "Thursday":
+          inProgressDate.thursday++;
+          break;
+        case "Friday":
+          inProgressDate.friday++;
+          break;
+        case "Saturday":
+          inProgressDate.saturday++;
+          break;
+        case "Sunday":
+          inProgressDate.sunday++;
+          break;
+        default:
+          break;
+      }
+    });
+    const notProcessed = tickets.filter(
+      (ticket) => ticket.status === "not processed"
+    );
+    notProcessed.forEach((ticket) => {
+      switch (ticket.date) {
+        case "Monday":
+          notProcessedDate.monday++;
+          break;
+        case "Tuesday":
+          notProcessedDate.tuesday++;
+          break;
+        case "Wednesday":
+          notProcessedDate.wednesday++;
+          break;
+        case "Thursday":
+          notProcessedDate.thursday++;
+          break;
+        case "Friday":
+          notProcessedDate.friday++;
+          break;
+        case "Saturday":
+          notProcessedDate.saturday++;
+          break;
+        case "Sunday":
+          notProcessedDate.sunday++;
+          break;
+        default:
+          break;
+      }
+    });
+    setNotProcessed(notProcessedDate);
+    setInProgress(inProgressDate);
+    setDone(doneDate);
   };
   useEffect(() => {
     getTickets();
   }, []);
   console.log(tickets);
   console.log(done);
+  console.log(inProgress);
+  console.log(notProcessed);
   return (
     <>
       <div className="header">
         <h1 className="title">Pie Chart</h1>
       </div>
 
-      <div style={{ width: "30vw", border: "1px solid black" }}>
+      <div style={{ width: "50vw", border: "1px solid black" }}>
         <Line data={data} />
       </div>
     </>
