804b96d0fc8d1407f83a8464a833269f0a6be67a
Chuong Nguyen <quyenchuong1998@gmail.com>
2021-08-23 22:29:36 +0700

    added redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,13 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import logger from "redux-logger";
 
-const initialState ={};
+const initialState = {};
 const store = createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(thunk))
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(logger, thunk))
 );
 
 export default store;
