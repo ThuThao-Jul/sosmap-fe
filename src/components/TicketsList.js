fdd81ec44a40a73239c5499614f0346fd66d3c04
Thu Thao <pounguyen.nttts@gmail.com>
2021-08-25 01:02:22 +0700

    tickets list filter

--- src/components/TicketsList.js
+++ src/components/TicketsList.js
@@ -3,10 +3,19 @@ import { useState, useEffect } from "react";
 import api from "../redux/axios";
 import { Card, Col, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Divider, Tag } from "antd";
+import { Divider, Tag, Select, Button } from "antd";
+import tagRender from "./TagRender";
+
+const optionsStatus = [{ value: 'gold', label: 'NOT PROCESSED' }, { value: 'lime', label: 'IN PROGRESS' }, { value: 'green', label: 'DONE' }];
+const optionsPriority = [{ value: 'gold', label: 'HIGH' }, { value: 'lime', label: 'MEDIUM' }, { value: 'green', label: 'LOW' }];
+const { Option } = Select;
 
 const TicketsList = () => {
   const [ticketList, setTicketList] = useState();
+  const [filter, setFilter] = useState({"status": [], "priority": [], "area": []});
+  const [dataFilter, setDataFilter] = useState([]);
+ 
+  
   useEffect(() => {
     const ticketDetail = async () => {
       try {
@@ -20,10 +29,72 @@ const TicketsList = () => {
     };
     ticketDetail();
   }, []);
+
+  const children = [];
+  for (let i = 1; i <= 10; i++) {
+    children.push(<Option key={"Quận " + i}>{"Quận "+ i}</Option>);
+  }
+
+  const handleChange = (value) => {
+  // console.log(`selected ${value}`);
+  setFilter({...filter, "area": value})
+  }
+
+  const handleStatus = (value, label) => {
+     setFilter({...filter, "status": label.map((l) => l.label.toLowerCase())})
+  }
+
+  const handlePriority = (value, label) => {
+    setFilter({...filter, "priority": label.map((l) => l.label.toLowerCase())})
+  }
+
+  const handleFilter = (e) => {
+    e.preventDefault();
+    const x = ticketList.filter((t) => 
+    (filter.status.length>0 ? filter.status.includes(t.status) : true ) && 
+    (filter.priority.length>0 ? filter.priority.includes(t.priority) : true) && 
+    (filter.area.length>0 ? filter.area.includes(t.district) : true ));
+    
+    setDataFilter(x)
+  }
+
+  console.log(filter)
+  console.log(dataFilter)
   return (
     <Row>
-      {ticketList &&
-        ticketList.map((t, index) => (
+   
+ <Row style={{border:"1px solid gray", marginTop:"3%", marginBottom:"3%", borderRadius:"5px"}}>
+   <Col>
+   <Select
+    mode="multiple"
+    showArrow
+    tagRender={tagRender}
+    style={{ width: '90%', margin:"3%" }}
+    options={optionsStatus}
+    placeholder ='Choose status'
+    onChange={handleStatus}
+  />
+</Col>
+    <Col>  
+<Select
+    mode="multiple"
+    showArrow
+    tagRender={tagRender}
+    style={{ width: '90%', margin:"3%" }}
+    options={optionsPriority}
+    placeholder ='Choose priority'
+    onChange = {handlePriority}
+  />
+    </Col>
+    <Col>
+    <Select mode="tags" style={{ width: '90%', margin:"3%" }} placeholder="Choose area to search" onChange={handleChange}>
+    {children}
+  </Select>
+    </Col>
+    <Button type="primary" style={{width:"10%", margin:"0.8%"}} onClick={handleFilter}>OK</Button>
+</Row>
+      { (dataFilter.length>0 ? dataFilter : ticketList) &&
+        (dataFilter.length>0 ? dataFilter : ticketList).map((t, index) => (
           <Col style={{ marginBottom: "40px" }}>
             <Card style={{ width: "18rem" }}>
               <Card.Body>
