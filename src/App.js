6eaee39197126e2088376f32315c23ee97e0459e
Thu Thao <pounguyen.nttts@gmail.com>
2021-08-24 15:49:45 +0700

    route fixed

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
     <Router>
       <DashboardPage />
       <Switch>
-        <Route exact path="/" component={DashboardPage} />
+        {/* <Route exact path="/" component={DashboardPage} /> */}
           {/* <Route exact path="/ticketsList" component={TicketsList} />
           <Route exact path="/itemsType" component={PieChart} />
           <Route exact path="/ticketsDistrict" component={PieChartStatus}/>
           <Route exact path="/ticketsStatus" component={BarChart} />
           <Route exact path="/ticketsDate" component={LineChart} /> */}
-          <Route component={NotFoundPage} />
+          {/* <Route component={NotFoundPage} /> */}
       </Switch>
     </Router>
   );
