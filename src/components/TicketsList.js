91d0ef13f75c7ecd886958e95b918784f8f4aa39
Chuong Nguyen <quyenchuong1998@gmail.com>
2021-08-24 22:16:22 +0700

    item list decoration

--- src/components/TicketsList.js
+++ src/components/TicketsList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import api from "../redux/axios";
-import { Card } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Divider, Tag } from "antd";
 
 const TicketsList = () => {
   const [ticketList, setTicketList] = useState();
@@ -20,45 +21,55 @@ const TicketsList = () => {
     ticketDetail();
   }, []);
   return (
-    <div>
+    <Row>
       {ticketList &&
         ticketList.map((t, index) => (
-          <div>
+          <Col style={{ marginBottom: "40px" }}>
             <Card style={{ width: "18rem" }}>
               <Card.Body>
-                <Card.Title>
-                  {index + 1}: {t.name}
-                </Card.Title>
+                <Card.Title>{t.name}</Card.Title>
                 <Card.Text>Address: {t.address}</Card.Text>
                 <Card.Text>Area: {t.district}</Card.Text>
+                <Divider></Divider>
                 <Card.Text>
                   In need:
                   {t.items.map((items) => (
                     <li>
                       {items.name}:{" "}
                       {items.name === "Mì Gói" ? (
-                        <span>{items.quantity + " box"}</span>
+                        <span>{items.quantity + " thùng"}</span>
                       ) : items.name === "Trứng" ? (
-                        <span>{items.quantity + " blister"}</span>
+                        <span>{items.quantity + " vỉ"}</span>
                       ) : items.name === "Gạo" ? (
                         <span>{items.quantity + " kg"}</span>
                       ) : items.name === "Sữa" ? (
-                        <span>{items.quantity + " package"}</span>
+                        <span>{items.quantity + " lốc"}</span>
                       ) : items.name === "Quần Áo" ? (
-                        <span>{items.quantity + " set"}</span>
+                        <span>{items.quantity + " bộ"}</span>
                       ) : (
-                        <span>{items.quantity + " bottle"}</span>
+                        <span>{items.quantity + " chai"}</span>
                       )}
                     </li>
                   ))}
                 </Card.Text>
-                <Card.Text>Priority: {t.priority}</Card.Text>
-                <Card.Text>Status: {t.status}</Card.Text>
+                <Card.Text>Priority: {t.priority.toUpperCase()}</Card.Text>
+                <Card.Text>
+                  {" "}
+                  {t.status === "not processed" ? (
+                    <Tag color="#F7464A">{t.status.toUpperCase()}</Tag>
+                  ) : t.status === "in progress" ? (
+                    <Tag color="rgba(255, 208, 0, 1)">
+                      {t.status.toUpperCase()}
+                    </Tag>
+                  ) : (
+                    <Tag color="#46BFBD">{t.status.toUpperCase()}</Tag>
+                  )}
+                </Card.Text>
               </Card.Body>
             </Card>
-          </div>
+          </Col>
         ))}
-    </div>
+    </Row>
   );
 };
 
